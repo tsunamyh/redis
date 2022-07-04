9e4e3e30258e02a2e240b64647fe6978d992b3df
Hojat <fathpour.hojat@gmail.com>
2022-07-05 01:59:56 +0430

    firs commit redis

---
+++ redis.js
@@ -0,0 +1,41 @@
+const client = require('redis').createClient();
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 4000;
+
+let isArray = function(a) {
+    return (!!a) && (a.constructor === Array);
+};
+client.connect().then(() => {
+    console.log('Redis client connected');
+})
+
+app.get('/data/:searchtext', async (req, res) => {
+    const searchtext = req.params.searchtext;
+    client.on('error', (err) => console.log('Redis Client Error:::::'));
+    console.log("searchtext: ", searchtext);
+    const value = JSON.parse(await client.get(searchtext))
+    console.log("value::", isArray(value),typeof(value));
+
+    if (isArray(value)) {
+        console.log('Redis Cache Hit');
+        res.json({
+            message: 'Redis Cache Hit',
+            data: value
+        });
+    } else {
+        console.log('Redis Cache Miss');
+        const response = await axios.get(`https://api.gateio.ws/api/v4/futures/usdt/${searchtext}`);
+        const data = response.data;
+        client.set(searchtext, JSON.stringify(data));
+        res.send({
+            error: false,
+            message: `Data for ${searchtext} from the server`,
+            data: data
+        });
+    }
+})
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
